ce4b7895ae78edd12346a8114b35c3079d741976
drwill <drwillsmith@gmail.com>
2019-12-03 10:14:45 -0600

    Initial Commit

---
+++ GuildChallenge1/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  GuildChallenge1
+//
+//  Created by William Smith on 11/29/19.
+//  Copyright © 2019 William Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var userInput: String = ""
+    @State var result: Double = 0.0
+    
+    var body: some View {
+        VStack{
+            Spacer()
+        TextField(//prompt, //variable to get user input)
+            Button(action: { // call func to do calculation}) {
+                Text("Convert")
+            }
+            Spacer()
+            Text(//print result)
+            Spacer()
+        }.padding(40)
+    }
+    
+    func convertTemp() {
+        var tempToConvert: Double
+        tempToConvert = Double(userInput)!
+        
+        result = (tempToConvert - 32.0) * (5.0/9.0)
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
